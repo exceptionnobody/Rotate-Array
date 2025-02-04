14d2461da5771d7dfe4fe3375f1fd0098fb56063
Francesco Galazzo <72932124+exceptionnobody@users.noreply.github.com>
2025-02-04 16:43:57 -0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int size = 10;
+        CircularArray<String> array = new CircularArray<String>(size);
+        for (int i = 0; i < size; i++) {
+            array.set(i, String.valueOf(i));
+        }
+
+        array.rotate(-3);
+
+        for (int i = 0; i < size; i++) {
+            System.out.println(array.get(i));
+        }
+
+        System.out.println("");
+
+        array.rotate(2);
+        for (String s : array) {
+            System.out.println(s);
+        }
+    }
+
+}
